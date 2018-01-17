e4071d24a0d9fe446296e8df19338e25c13c76e4
Juan Moran <moranjuanpedro@gmail.com>
2018-01-17 12:05:11 -0800

    It works
    it's good for both joysticks and controller.

--- src/org/usfirst/frc/team7077/robot/Robot.java
+++ src/org/usfirst/frc/team7077/robot/Robot.java
@@ -20,19 +20,19 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  */
 public class Robot extends IterativeRobot {
 	private DifferentialDrive m_myRobot;
-	private Joystick m_leftStick;
-	private Joystick m_rightStick;
+	//private Joystick m_leftStick;
+	//private Joystick m_rightStick;
 	private XboxController m_controller;
 
 	public void robotInit() {
 		m_myRobot = new DifferentialDrive(new Spark(0), new Spark(2));
-		//m_leftStick = new Joystick(0);
+		//m_leftStick = new Joystick(0); //Use this and bottom when using TWO joysticks
 		//m_rightStick = new Joystick(1);
-		m_controller = new XboxController(0);
+		m_controller = new XboxController(0); //Use this when using single controller
 	}
 
 	public void teleopPeriodic() {
-		m_myRobot.tankDrive(m_controller.getY(Hand.kLeft), m_controller.getY(Hand.kRight));
+		m_myRobot.tankDrive(m_controller.getY(Hand.kRight), m_controller.getY(Hand.kLeft));
 		//m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
 	}
 }
