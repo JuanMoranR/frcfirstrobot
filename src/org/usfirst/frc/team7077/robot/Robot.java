be167652839648bb8ca229c21b5be889345eee4f
Juan Moran <moranjuanpedro@gmail.com>
2018-02-10 13:03:43 -0800

    more good
    finished controller speed settings switch case.  Started on if statement for changing whether controller or joysticks.

--- src/org/usfirst/frc/team7077/robot/Robot.java
+++ src/org/usfirst/frc/team7077/robot/Robot.java
@@ -11,32 +11,29 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
+
 
 public class Robot extends IterativeRobot {
+	
+	private static final int controllerType = 0; //0 for Logitech controller, 1 for joysticks
 	private DifferentialDrive m_myRobot;
-	//private Joystick m_leftStick;
-	//private Joystick m_rightStick;
+	private Joystick m_leftStick;
+	private Joystick m_rightStick;
 	private XboxController m_controller;
 	
 	private static final int kMotorPortLeft = 0; //Change this to whatever the left motor port is on
 	private static final int kMotorPortRight = 1; //Change this to whatever the right motor port is on
 	
 	//Talons for intake wheels
-	Talon m_frontLeft = new Talon(2);
-	Talon m_frontRight = new Talon(3);
-	SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft);
-	SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight);
+	Talon m_arm = new Talon(2);
+	Talon m_frontLeft = new Talon(3);
+	Talon m_frontRight = new Talon(4);
 			
-	DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+	DifferentialDrive m_drive = new DifferentialDrive(m_frontLeft, m_frontRight);
 	
 	private static final int kJoystickPortLeft = 0; //Change this to whatever the left joystick port is on
 	private static final int kJoystickPortRight = 1; //Change this to whatever the right joystick port is on
@@ -53,15 +50,7 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopPeriodic() {
-		/** 
-		* WIP for DPAD speed toggle (slow, medium, fast, all out)
-		* Have two if else statements, one that outputs a number between 0 and 3
-		* Depending on which DPAD button is pressed
-		* Where 0 is the slowest and 3 is the fastest
-		* The next if else checks the number set in the first statement and
-		* Sets the max speed to slow or fast
-		* What follows is pseudocode
-		*/
+		
 		int speed = 3;
 		if (m_controller.getXButton() == true)
 			speed = 0;
@@ -71,28 +60,108 @@ public class Robot extends IterativeRobot {
 			speed = 2;
 		if (m_controller.getAButton() == true)
 			speed = 3;
-			
+		
+		if (controllerType == 0)
+		{	
 		switch(speed)
 		{
 			case 0:
-				if (m_controller.getY(Hand.kLeft) == 1)
-					m_myRobot.tankDrive(0.25, 0);
-				if (m_controller.getY(Hand.kRight) == 1)
-					m_myRobot.tankDrive(0, 0.25);
-				if (m_controller.getY(Hand.kLeft) == 1 && m_controller.getY(Hand.kRight) == 1)
+				if (m_controller.getPOV()==0)
+					m_myRobot.tankDrive(-0.25,-0.25);
+				if (m_controller.getPOV()==180)
 					m_myRobot.tankDrive(0.25, 0.25);
-				
+				if (m_controller.getY(Hand.kLeft) == 1)
+					m_myRobot.tankDrive(-0.25, 0);
+				if (m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(0, -0.25);
+				if (m_controller.getY(Hand.kLeft) == 1 && m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(-0.25, -0.25);
+				if (m_controller.getY(Hand.kLeft) == -1)
+					m_myRobot.tankDrive(0.25, 0);
+				if (m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0, 0.25);
+				if (m_controller.getY(Hand.kLeft) == -1 && m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0.25, 0.25);
 			case 1:
-				set robot tank drive to medium;
+				if (m_controller.getPOV()==0)
+					m_myRobot.tankDrive(-0.5,-0.5);
+				if (m_controller.getPOV()==180)
+					m_myRobot.tankDrive(0.5, 0.5);
+				if (m_controller.getY(Hand.kLeft) == 1)
+					m_myRobot.tankDrive(-0.5, 0);
+				if (m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(0, -0.5);
+				if (m_controller.getY(Hand.kLeft) == 1 && m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(-0.5, -0.5);
+				if (m_controller.getY(Hand.kLeft) == -1)
+					m_myRobot.tankDrive(0.5, 0);
+				if (m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0, 0.5);
+				if (m_controller.getY(Hand.kLeft) == -1 && m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0.5, 0.5);
 			case 2:
-				set robot tank drive to fast;
+				if (m_controller.getPOV()==0)
+					m_myRobot.tankDrive(-0.75,-0.75);
+				if (m_controller.getPOV()==180)
+					m_myRobot.tankDrive(0.75, 0.75);
+				if (m_controller.getY(Hand.kLeft) == 1)
+					m_myRobot.tankDrive(-0.75, 0);
+				if (m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(0, -0.75);
+				if (m_controller.getY(Hand.kLeft) == 1 && m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(-0.75, -0.75);
+				if (m_controller.getY(Hand.kLeft) == -1)
+					m_myRobot.tankDrive(0.75, 0);
+				if (m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0, 0.75);
+				if (m_controller.getY(Hand.kLeft) == -1 && m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0.75, 0.75);
 			case 3:
 				m_myRobot.tankDrive(m_controller.getY(Hand.kRight), m_controller.getY(Hand.kLeft));
 			default:
 				System.out.println("Somethings wrong, speed variable isnt set");
 		}
+		}
+		else if (controllerType == 1)
+		{
+			switch(speed)
+		{
+			case(0):
+			{
+				if (m_leftStick.getPOV()==0)
+					m_myRobot.tankDrive(-0.25,-0.25);
+				if (m_leftStick.getPOV()==180)
+					m_myRobot.tankDrive(0.25, 0.25);
+				if (m_controller.getY() == 1)
+					m_myRobot.tankDrive(-0.25, 0);
+				if (m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(0, -0.25);
+				if (m_controller.getY(Hand.kLeft) == 1 && m_controller.getY(Hand.kRight) == 1)
+					m_myRobot.tankDrive(-0.25, -0.25);
+				if (m_controller.getY(Hand.kLeft) == -1)
+					m_myRobot.tankDrive(0.25, 0);
+				if (m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0, 0.25);
+				if (m_controller.getY(Hand.kLeft) == -1 && m_controller.getY(Hand.kRight) == -1)
+					m_myRobot.tankDrive(0.25, 0.25);
+			}
+			case(1):
+			{
+				//case 1
+			}
+			case(2):
+			{
+				//case 2
+			}
+			case(3):
+			{
+				m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+			}
+		}
+		}
+
 			
-		
+		/*
 		m_myRobot.tankDrive(m_controller.getY(Hand.kRight), m_controller.getY(Hand.kLeft)); //Use this when using one controller
 		//System.out.println(m_controller.getY(Hand.kRight));
 		//m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY()); //Use this when using TWO joysticks
@@ -102,5 +171,6 @@ public class Robot extends IterativeRobot {
 			m_myRobot.tankDrive(1, 1);
 		m_frontLeft.set(m_controller.getRawAxis(3));
 		System.out.println(m_frontLeft);
+		*/
 	}
 }
