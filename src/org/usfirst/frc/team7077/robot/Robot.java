7549d098511e7fb7e45f5bc74ce9c31042b52b80
Juan Moran <moranjuanpedro@gmail.com>
2018-08-02 12:14:08 -0700

    08/02/18
    Changes made with Dylan.

--- src/org/usfirst/frc/team7077/robot/Robot.java
+++ src/org/usfirst/frc/team7077/robot/Robot.java
@@ -8,62 +8,63 @@
 package org.usfirst.frc.team7077.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.*;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
-
+//PLEASE READ!!!! 11.2-12 if We 4 batteries otherwise do 11.6-12
 public class Robot extends IterativeRobot {
 	
 	public static DifferentialDrive m_myRobot;
 	public XboxController m_controller;
-	
-	Timer timer;
-	
-	private static final int kMotorPortLeft = 0; //Change this to whatever the left motor port is on
-	private static final int kMotorPortRight = 1; //Change this to whatever the right motor port is on
-	
-	//Talon for arm
-	TalonSRX m_arm = new TalonSRX(2); //Port # on the RoboRio
-	//Talon for intake wheels
-	Talon m_Lintake = new Talon(3);
-	Talon m_Rintake = new Talon(4);
-	
-	public static final int kControllerPort = 5; //Change this to whatever the ONE controller port is on
+	public Joystick m_Right;
+	public Joystick m_Left;
+	//Timers
+	Timer autoTime;	//Timer for the Autonomous
+	Timer oneEightyTime;	//Timer for testing the 
 	
 	
-	Spark m_leftDrive = new Spark(kMotorPortLeft);
-	Spark m_rightDrive = new Spark(kMotorPortRight);
+	//Ports 
+	//Ports are intergers 0 is the right most port whilst 1 is right next to the 0 port.
+	private static final int kMotorPortLeft = 0; 
+	private static final int kMotorPortRight = 1; 
+	
+	public static final int kControllerPort = 0; //Change this integer to which ever the port the controller is plugged into (0-9)
+	
+	//Motors for the Left and Right Drivers.
+	Spark m_leftDrive = new Spark(kMotorPortLeft);	//Spark Motors for the Left Wheels
+	Spark m_rightDrive = new Spark(kMotorPortRight); //Spark Motors for the Right Wheels
+	
+	
+	//JOYSTICKS
+
+	
+	//Doubles for Accelerometers.
+	double LDTMotor;
+	double RDTMotor;
+
 	
 	public void robotInit() {
-		//Spark 0 and 1 are drive motors, 0 is 2 left side 1 is 2 right side.
-		m_myRobot = new DifferentialDrive(m_leftDrive, m_rightDrive);
-
-		//m_leftStick = new Joystick(kJoystickPortLeft); //Use this and bottom when using TWO joysticks
-		//m_rightStick = new Joystick(kJoystickPortRight);
-		m_myRobot.setExpiration(0.1);
-		m_controller = new XboxController(kControllerPort); //Use this when using single controller
-		
-		timer = new Timer();
+		//Spark 0 and 1 are drive motors, 0 is the 2 left side 1 is the 2 right side.
+		//Spark 0 and 1 are drive motors, 0 is the 2 left side 1 is the 2 right side.
+				m_myRobot = new DifferentialDrive(m_leftDrive, m_rightDrive);	//Calls both the "left" and "Right" motors for the myRobot function upon Robot Initiation
+				m_controller = new XboxController(kControllerPort); //Call This when using a Controller.
+				m_Right = new Joystick(3);	// Right Joystick port goes into the 3rd Port in the Driver Station
+				m_Left = new Joystick(4);	// Left Joystick Port goes into the 4th Port in the Driver Station
 	}
 	
 
 	@Override
 	public void autonomousInit() {
-		m_myRobot.setSafetyEnabled(false);
-		m_leftDrive.setSafetyEnabled(false);
-		m_rightDrive.setSafetyEnabled(false);
-
-		timer.reset();
-		timer.start();
-	
+		
+		autoTime = new Timer();
 		
 	}
 
@@ -72,68 +73,123 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		if (timer.get()<=2) {
-			m_myRobot.tankDrive(-0.675, -0.675);
+		if (autoTime.get()<=.9965) {
+			m_myRobot.tankDrive(-1, -1);
 		}
-		
-		else {
+		else if (autoTime.get()==1.1) {
 			m_myRobot.tankDrive(0, 0);
 		}
-		Timer.delay(0.005);
+		
+		
+		
+		//NOTES FOR AUTONOMOUS
+//		/*if (timer.get()<=1) {
+//			m_myRobot.tankDrive(-1, -1);	// 1,1 is backwards so -1,-1 should be forwards
+//			m_Lintake.set(-1);
+//			m_Rintake.set(1);
+//		}
+//		else if (timer.get()==1.1) {
+//			m_myRobot.tankDrive(0,0);
+//		}
+//		else if (timer.get()<=1.8) {
+//			m_myRobot.tankDrive(-1,1);
+//			m_Lintake.set(-1);
+//			m_Rintake.set(1);
+//		}
+//		else if (timer.get()==1.9) {
+//			m_myRobot.tankDrive(0,0);
+//		
+//		//else if (timer.get()<=.5) {
+//		//m_myRobot.tankDrive(0, 0);
+//		//.9965 is the time it takes for a full 360 turn		
+//		//}
+//		
+//		else if (timer.get()<=2) {
+//			m_Lintake.set(-1);
+//			m_Rintake.set(1);
+//			m_myRobot.tankDrive(-1, -1);
+//		}
+//		
+//		else if (timer.get()<=3) {
+//			m_myRobot.tankDrive(-.7, -.7);
+//			m_Lintake.set(1);
+//			m_Rintake.set(-1);
+//			
+//			
+//		}
+//		else if (timer.get()<=4) {
+//			m_Lintake.set(-1);
+//			m_Rintake.set(1);
+//		}
+//		
+//		else if (timer.get()<=15){
+//			//m_Rintake.set(-1);
+//			//m_Lintake.set(1);
+//			m_myRobot.tankDrive(0, 0);
+//		}
+//		*/
+//		
+//		if (timer.get() <=1) {
+//			m_myRobot.tankDrive(1,-1);
+//		}
+//		else {
+//			m_myRobot.tankDrive(0,0);
+//		}
+//		Timer.delay(0.005);
 	}
-
 	@Override
 	public void teleopInit() {
 	}
 	@Override
 	public void teleopPeriodic() {
 		
-		m_myRobot.tankDrive(m_controller.getY(Hand.kRight), m_controller.getY(Hand.kLeft));
-		if (m_controller.getYButtonPressed()==true)
-		{	
-			m_Lintake.set(-1);
-			m_Rintake.set(1);
-		}	
-		if (m_controller.getYButtonReleased()==true)
-		{
-			m_Lintake.set(0);
-			m_Rintake.set(0);
-		}	
-		if (m_controller.getAButtonPressed()==true)
-		{
+		LDTMotor = m_controller.getY(Hand.kRight);
+		RDTMotor = m_controller.getY(Hand.kLeft);
 		
-			m_Lintake.set(1);
-			m_Rintake.set(-1);
-		}
-		if (m_controller.getAButtonReleased()==true)
-		{	
-			m_Lintake.set(0);
-			m_Rintake.set(0);
-		}	
+		m_myRobot.tankDrive(RDTMotor/1.3, LDTMotor/1.3);
+		
+		
+//		if (m_controller.getBumperPressed(Hand.kLeft) == true)
+//		{	
+//			m_Lintake.set(1);
+//			m_Rintake.set(-1);
+//		}	
+//		if (m_controller.getBumperReleased(Hand.kLeft) == true)
+//		{
+//			m_Lintake.set(0);
+//			m_Rintake.set(0);
+//		}	
+//		if (m_controller.getBumperPressed(Hand.kRight) == true)
+//		{
+//		
+//			m_Lintake.set(-1);
+//			m_Rintake.set(1);
+//		}
+//		if (m_controller.getBumperReleased(Hand.kRight) == true)
+//		{	
+//			m_Lintake.set(0);
+//			m_Rintake.set(0);
+//		}	
 		
 		if (m_controller.getPOV()==0)
-			m_myRobot.tankDrive(-1,-1);
+			m_myRobot.tankDrive(-0.8,-0.8);
 		if (m_controller.getPOV()==180)
-			m_myRobot.tankDrive(1, 1);
+			m_myRobot.tankDrive(0.8, 0.8);
 		if (m_controller.getPOV()==90)
-			m_myRobot.tankDrive(-1, 1);
+			m_myRobot.tankDrive(-0.8, 0.8);
 		if (m_controller.getPOV()==270)
-			m_myRobot.tankDrive(1, -1);
-		
-		if (m_controller.getBumperPressed(Hand.kLeft) == true)
-			m_arm.set(ControlMode.PercentOutput, -0.4);
-		if (m_controller.getBumperReleased(Hand.kLeft) == true)
+			m_myRobot.tankDrive(0.8, -0.8);
+		/*
+		if (m_controller.getYButtonPressed() == true)
+			m_arm.set(ControlMode.PercentOutput, -1);
+		if (m_controller.getYButtonReleased() == true)
 			m_arm.set(ControlMode.PercentOutput, 0);
-		if (m_controller.getBumperPressed(Hand.kRight) == true)
+		if (m_controller.getBButtonPressed() == true)
 			m_arm.set(ControlMode.PercentOutput, 1);
-		if (m_controller.getBumperReleased(Hand.kRight) == true)
+		if (m_controller.getBButtonReleased() == true)
 			m_arm.set(ControlMode.PercentOutput, 0);
-		
-		Timer.delay(0.005); // wait for a motor update time
-	}
-	@SuppressWarnings("deprecation")
-	@Override
-	public void testPeriodic() {
-		LiveWindow.run();
+		*/
+		//m_arm.set(ControlMode.PercentOutput, m_controller.getX(Hand.kRight));
+		//Timer.delay(0.005); // wait for a motor update time
 	}
 }
